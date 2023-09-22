ba314281334f80ad706503a5af76e6dad2cf96bd
AN-dragon <alan.orisio@etec.sp.gov.br>
2023-09-22 17:21:04 +0000

    Controlador

---
+++ app/src/main/java/application/controller/PlataformaController.java
@@ -0,0 +1,40 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import application.model.Plataforma;
+import application.model.PlataformaRepository;
+
+@Controller
+@RequestMapping("/plataforma")
+public class PlataformaController {
+    @Autowired
+    private PlataformaRepository plataformaRepo;
+
+    @RequestMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("plataformas", plataformaRepo.findAll());
+        return "/platforma/lista";
+    }
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/plataforma/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("nome") String nome, @RequestParam("fornecedor") String fornecedor) {
+        Plataforma plataforma = new Plataforma();
+        plataforma.setNome(nome);
+        plataforma.setFornecedor(fornecedor);
+
+        plataformaRepo.save(plataforma);
+
+        return "redirect:/plataforma/list";
+    }
+}
